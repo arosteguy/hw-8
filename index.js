fde88012ca64fa9844656f2b91535959d3dc6351
cake <larosteguy@gmail.com>
2020-01-15 21:39:47 -0700

    setting up npm

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+   prompt.rawlist({
+
+    })
+
+
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
