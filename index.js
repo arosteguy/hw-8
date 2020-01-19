d553d98e79af8cee3957028c445b8302570812ef
cake <larosteguy@gmail.com>
2020-01-18 20:33:30 -0700

    added css file start html

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-const fs = require("fs");
-const axios = require("axios");
-const inquirer = require("inquirer");
-const Prince = require("prince");
 
-inquirer
-    .prompt([
-    {
-    type: 'input',
-    message: "What is your GitHub username?",
-    name: "username"
-    },
-
-    {
-    type: 'rawlist',
-    message: "What color do you want?",
-    choices:[" green", " blue"," pink", " red", ],
-    name: "colors"
-    },
     
-])
-    .then(function ({ username, colors }) {
-        const queryUrl = `https://api.github.com/users/${username}?client_id=undefined&client_secret=undefined`;
-        axios.get(queryUrl).then(({ data }) => {
-            console.log(data);
-        });
-        console.log(colors)
-    })
-    .catch(function(err){
-        console.log(err);
-    })
-    
-    fs.writeToFile(`${colors}.txt`, data, (err) => {
+    // fs.writeToFile(`${colors}.txt`, data, (err) => {
 
-        if (err) throw err;
-        console.log(`Wrote ${colors}.text`);
-     })
+    //     if (err) throw err;
+    //     console.log(`Wrote ${colors}.text`);
+    //  })
+    module.exports();
