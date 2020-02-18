84090c74516395eee94ccbe8e5b2baf1a943e71e
cake <larosteguy@gmail.com>
2020-02-17 21:02:00 -0700

    added api.js

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # hw-8
 node/ HTML  convert to PDF
+
+#  Node HTML Conversion  
+##  General Info  
+Create a command-line application that dynamically generates a PDF profile from a GitHub username  
+## Technologies  
+Node
+HTML
+JavaScript
+### Dependencies  
+FS
+Axios
+Inquirer
+Prince
+Util  
+## Author  
+Lori Arosteguy
--- index.js
+++ index.js
@@ -1,58 +1,98 @@
-const fs = require("fs");
-const axios = require("axios");
-const inquirer = require("inquirer");
-const Prince = require("prince");
-const util   = require("util")
+ const fs = require("fs");
+ const axios = require("axios");
+ const inquirer = require("inquirer");
+ const api = require("./api");
+ const path = require("path");
 
  const generateHTML = require("./generateHTML")
-let axiosResponse;
+ const open = require("open");
+ const conversionForm = require("electron-html-to");
+ let axiosResponse;
 
-inquirer
-    .prompt([
-        {
-        type: 'input',
-        message: "What is your GitHub username?",
-        name: "username"
-        },
-        {
-        type: 'rawlist',
-        message: "What color do you want?",
-        choices:[" green", " blue"," pink", " red", ],
-        name: "colors"
-        }, 
-    ])
-    .then(function ({ username, colors }) {
-        const queryUrl = `https://api.github.com/users/${username}?client_id=undefined&client_secret=undefined`;
-         axios
-            .get(queryUrl)
-            .then(({ data }) => {
-                axiosResponse = JSON.stringify(data);
-                fs.writeFileSync(`${colors}.txt`, axiosResponse, (err) => {
 
-                    if (err) throw err;
-                    console.log(`Wrote ${colors}.text`);
-                  })
-                Prince()
-                    .inputs(`${colors}.txt`)
-                    .output("test.pdf")
-                    .execute()
-                    .then(function (generateHTML) {
-                        console.log("OK: done")
-                    })
-                    .catch( function (error) {
-                        console.log("ERROR: ", util.inspect(error))
-                    })
-            })
-            .catch(function(err){
-                console.error(err);
-             })
+
+// inquirer.prompt([
+//         {
+//         type: 'input',
+//         message: "What is your GitHub username?",
+//         name: "username"
+//         },
+//         {
+//         type: 'rawlist',
+//         message: "What color do you want?",
+//         choices:[" green", " blue"," pink", " red", ],
+//         name: "colors"
+//         }, 
+//     ])
+//     .then(function ({ username, colors }) {
+//         const queryUrl = 'https://api.github.com/users/${username}?client_id=undefined&client_secret=undefined';
+//          axios
+//             .get(queryUrl)
+//             .then(({ data }) => {
+//                 axiosResponse = JSON.stringify(data);
+//                 fs.writeFileSync(`${colors}.txt`, axiosResponse, (err) => {
+
+//                     if (err) throw err;
+//                     console.log(`Wrote ${colors}.text`);
+//                   })
+            
+//             .catch(function(err){
+//                 console.error(err);
+//              })
         
-     })
-    
-     .catch(function(err){
-        console.log(err);
-     })
-   
+//      });
+const questions = [
+    {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+    },
+    {
+    type: "rawlist",
+    message: "What color do you want?",
+    choices:[" green", " blue"," pink", " red", ],
+    name: "colors"
+    },
+];
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 
+}
+     function init(){
+         inquirer.prompt(questions).then(({ github, color }) => {
+             console.log("Searching..");
+         
+     api    
+        .getUser(github).then(response =>
+            api.getTotalStars(github)
+            .then(stars => {
+                return generateHTML({
+                    stars,
+                    color,
+                    ...response.data
+                });
+            })
+        )
+        .then(html => {
+            const conversion = conversionForm({
+                converterPath: conversionForm.converters.PDF
+            });
+
+            conversion({ html }, function(err, result) {
+                if (err) {
+                    return console.log(err);
+                }
+                result.stream.pipe(
+                    fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+
+                );
+                conversion.kill();
+            });
+
+            open(path.join(process.cwd(), "resume.pdf"));
+
+        });
+    });
+}
+init();
     
-  
